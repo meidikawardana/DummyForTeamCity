22612a85bc7392e8f1a96d5375e3f51dea4becba
Meidika Wardana <jdeep555.mw@gmail.com>
2024-07-23 16:04:48 +0700

    Trying TeamCity's build cache (3)

--- src/modules/Sep/pages/SepsGrid.vue
+++ src/modules/Sep/pages/SepsGrid.vue
@@ -4,7 +4,7 @@
       <q-toolbar>
         <q-btn flat dense round icon="menu" aria-label="Menu" />
 
-        <q-toolbar-title> SEP - Trying TeamCity's build cache (2)</q-toolbar-title>
+        <q-toolbar-title> SEP - Trying TeamCity's build cache (3)</q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
