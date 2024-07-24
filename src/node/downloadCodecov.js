d0cd53e5000e3e032a6ef3ac571a75c30863d619
Meidika Wardana <jdeep555.mw@gmail.com>
2024-07-24 22:23:35 +0700

    add console.logs to the downloadCodecov.js

--- src/node/downloadCodecov.js
+++ src/node/downloadCodecov.js
@@ -1,10 +1,12 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 
+console.log('Running: exec curl -Os https://cli.codecov.io/v0.4.4/linux/codecov');
 exec('curl -Os https://cli.codecov.io/v0.4.4/linux/codecov', (error, stdout, stderr) => {
   if (error) {
     console.error(`Error executing: ${error}`);
     return;
   }
+  console.log('make codecov executable');
   fs.chmodSync('codecov', '755');
 });
